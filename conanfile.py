f414208e6397b07338f50ce19aaf5bff59813e25
jromphf <jromphf@library.rochester.edu>
2019-05-14 11:48:42 -0400

    working with master git branch for openexr - lets see how it does with the next package

--- conanfile.py
+++ conanfile.py
@@ -16,7 +16,7 @@ class IlmBaseConan(ConanFile):
     options = {"shared": [True, False], "namespace_versioning": [True, False], "fPIC": [True, False]}
     requires = ("zlib/1.2.8@conan/stable")
     generators = "cmake"
-    default_options = "shared=False", "namespace_versioning=True", "fPIC=False"
+    default_options = "shared=True", "namespace_versioning=True", "fPIC=False"
 
     def config_options(self):
         if self.settings.os == 'Windows':
@@ -27,13 +27,10 @@ class IlmBaseConan(ConanFile):
             self.options.fPIC = True
 
     def source(self):
-        tools.download(
-            "https://github.com/openexr/openexr/releases/download/v{}/ilmbase-{}.tar.gz".format(self.version,
-                                                                                                self.version),
-            'ilmbase.tar.gz')
-        tools.untargz('ilmbase.tar.gz')
-        tools.replace_in_file("{}/ilmbase-{}/CMakeLists.txt".format(self.source_folder, self.version), "PROJECT ( ilmbase )",
-                              """PROJECT ( ilmbase )
+        # TODO use master branch of git because releases are no good with CMake
+        self.run("git clone https://github.com/openexr/openexr.git openexr")
+        tools.replace_in_file("{}/openexr/IlmBase/CMakeLists.txt".format(self.source_folder), "PROJECT(ilmbase VERSION ${ILMBASE_VERSION})",
+                              """PROJECT(ilmbase VERSION ${ILMBASE_VERSION})
 include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
 conan_basic_setup()
 set (CMAKE_CXX_STANDARD 11)""")
@@ -42,25 +39,26 @@ set (CMAKE_CXX_STANDARD 11)""")
         cmake = CMake(self)
 
         cmake.definitions.update(
-            {"OPENEXR_BUILD_SHARED": True,
-             "BUILD_ILMBASE_STATIC": True,
-             "OPENEXR_VERSION": self.version,
-             "OPENEXR_SOVERSION": self.version,
+            {"OPENEXR_BUILD_ILMBASE": True,
+             "OPENEXR_BUILD_OPENEXR": False,
+             "OPENEXR_BUILD_SHARED": self.options.shared,
+             "OPENEXR_BUILD_STATIC": not self.options.shared,
+             "OPENEXR_BUILD_PYTHON_LIBS": False,
+             "OPENEXR_NAMESPACE_VERSIONING": False,
+             "CMAKE_PREFIX_PATH": self.deps_cpp_info["zlib"].rootpath
              })
 
-        if "fPIC" in self.options.fields:
-            cmake.definitions["CMAKE_POSITION_INDEPENDENT_CODE"] = self.options.fPIC
+        cmake.definitions["CMAKE_POSITION_INDEPENDENT_CODE"] = self.options.fPIC
 
-        cmake.configure(source_dir="{}/ilmbase-{}".format(self.source_folder, self.version))
+        cmake.configure(source_dir="{}/openexr".format(self.source_folder))
         cmake.build(target="install")
 
-
     def package(self):
-        self.copy("*.h", dst="include/OpenEXR", src="ilmbase-{}/Half".format(self.version), keep_path=False)
-        self.copy("*.h", dst="include/OpenEXR", src="ilmbase-{}/Iex".format(self.version), keep_path=False)
-        self.copy("*.h", dst="include/OpenEXR", src="ilmbase-{}/IexMath".format(self.version), keep_path=False)
-        self.copy("*.h", dst="include/OpenEXR", src="ilmbase-{}/IlmThread".format(self.version), keep_path=False)
-        self.copy("*.h", dst="include/OpenEXR", src="ilmbase-{}/Imath".format(self.version), keep_path=False)
+        self.copy("*.h", dst="include/OpenEXR", src="openexr/IlmBase/Half", keep_path=False)
+        self.copy("*.h", dst="include/OpenEXR", src="openexr/IlmBase/Iex".format(self.version), keep_path=False)
+        self.copy("*.h", dst="include/OpenEXR", src="openexr/IlmBase/IexMath".format(self.version), keep_path=False)
+        self.copy("*.h", dst="include/OpenEXR", src="openexr/IlmBase/IlmThread".format(self.version), keep_path=False)
+        self.copy("*.h", dst="include/OpenEXR", src="openexr/IlmBase/Imath".format(self.version), keep_path=False)
         self.copy("IlmBaseConfig.h", dst="include/OpenEXR", src="config", keep_path=False)
         self.copy("*.so", dst="lib", src="package/lib", keep_path=False)
         self.copy("*.so.*", dst="lib", src="package/lib", keep_path=False)
@@ -70,9 +68,10 @@ set (CMAKE_CXX_STANDARD 11)""")
         self.copy("*.lib", dst="lib", src="package/lib", keep_path=False)
 
         self.copy("FindIlmBase.cmake", src=".", dst=".")
-        self.copy("license*", dst="licenses", src="ilmbase-{}".format(self.version), ignore_case=True, keep_path=False)
+        self.copy("license*", dst="licenses", src="openexr".format(self.version), ignore_case=True, keep_path=False)
 
     def package_info(self):
+        # TODO libs need to reflect namespace versioning
         if self.options.shared and self.settings.os == "Windows":
             self.cpp_info.defines.append("OPENEXR_DLL")
         self.cpp_info.includedirs = ['include', 'include/OpenEXR']
--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -6,13 +6,14 @@ import os
 class DefaultNameConan(ConanFile):
     settings = "os", "compiler", "arch", "build_type"
     generators = "cmake"
+
     def build(self):
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
 
     def imports(self):
-        self.copy(pattern="*.dll", dst="bin", src="bin")
+        self.copy(pattern="*.dll", dst="bin", src="lib")
         self.copy(pattern="*.dylib", dst="bin", src="lib")
 
     def test(self):
